cc22769fedc87b446fc2ffdbd392a33215b72589
Jessy <jessy.com>
2015-09-28 14:17:06 -0500

    Dinosaur program

---
+++ Dinosaur.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+	// write your code here
+        DinosaurSupervisor t_rex = new DinosaurSupervisor();
+        DinosaurSupervisor velociraptor = new DinosaurSupervisor();
+        t_rex.roar();
+        velociraptor.roar();
+
+    }
+}
---
+++ DinosaurSupervisor.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by Jessy on 9/28/2015.
+ */
+public class DinosaurSupervisor {
+
+    public void roar(){       System.out.println("GRRR!!!!!");    }
+}
